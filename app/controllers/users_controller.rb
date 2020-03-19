8ce8cf8a932689586aca7193ebfa28b4fa8aa53c
kinta0402 <goro0410@gmail.com>
2020-03-19 09:12:32 +0000

    9.2.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   private
 
     def user_params # 5.5.3
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
     
     # beforeフィルター
