73965ef0d7ea0797d1059caef97379f623c9153f
kinta0402 <goro0410@gmail.com>
2020-03-20 10:19:23 +0000

    Finish edit-attendances

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,40 @@ class ApplicationController < ActionController::Base
   include SessionsHelper # 親contに先を記載する事で、どのcontでも定義したメソッドが使える 6.3
   
   $days_of_the_week = %w{日 月 火 水 木 金 土} # 10.2 ｸﾞﾛｰﾊﾞﾙ変数を使用
+
+  # beforフィルター
+
+  # paramsハッシュからユーザーを取得します。
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # ログイン済みのユーザーか確認します。
+  def logged_in_user
+    unless logged_in?
+      store_location # ﾌﾚﾝﾄﾞﾘｰﾌｫﾜｰﾃﾞｨﾝｸﾞ
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
+    end
+  end
+  
+  # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+  def correct_user
+    # @user = User.find(params[:id])
+    # redirect_to(root_url) unless @user == current_user ↓へ 8.2.2
+    redirect_to(root_url) unless current_user?(@user)
+  end
+  
+  # システム管理権限所有かどうか判定します。
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
   
   # ページ出力前に1ヶ月分のデータの存在を確認・セットします。10.3
   def set_one_month 
     # @first_day = Date.current.beginning_of_month 10.4により下記へ
-    @first_day = params[:date].nil? ?
+    @first_day = params[:date].nil? ? # URL記載の日時を取得 ← ひらめいた!!
     Date.current.beginning_of_month : params[:date].to_date
     @last_day = @first_day.end_of_month
     one_month = [*@first_day..@last_day] # 対象の月の日数を代入します。
--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -12,6 +12,7 @@ module AttendancesHelper
   
   # 出勤時間と退勤時間を受け取り、在社時間を計算して返します。 10.8
   # attendanceヘルパーで定義したメソッドを、users/show.html にて使用している → 可能なのか？
+  # モデルを関連付けしている為可能？
   def working_times(start, finish)
     format("%.2f", (((finish - start) / 60) / 60.0))
   end
