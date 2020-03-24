9c4f7391625119886c3d407e42498216523f9163
kinta0402 <goro0410@gmail.com>
2020-03-24 05:02:02 +0000

    仕様書チェック終了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper # 親contに先を記載する事で、どのcontでも定義したメソッドが使える 6.3
-  
+
   $days_of_the_week = %w{日 月 火 水 木 金 土} # 10.2 ｸﾞﾛｰﾊﾞﾙ変数を使用
 
   # beforフィルター
--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,5 @@
 class AttendancesController < ApplicationController
+  include AttendancesHelper
   before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
@@ -29,21 +30,39 @@ class AttendancesController < ApplicationController
   
   def edit_one_month
   end
-  
-  # 11.3.2 11.2付近参照 ※よく考えれば理解できそう!!
+
   def update_one_month
-    ActiveRecord::Base.transaction do # トランザクションを開始します。
-      attendances_params.each do |id, item|
-        attendance = Attendance.find(id)
-        attendance.update_attributes!(item)
+    ActiveRecord::Base.transaction do
+      if attendances_invalid?
+        attendances_params.each do |id, item|
+          attendance = Attendance.find(id)
+          attendance.update_attributes!(item)
+        end
+        flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+        redirect_to user_url(date: params[:date])
+      else
+        flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+        redirect_to attendances_edit_one_month_user_url(date: params[:date])
       end
     end
-    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
-    redirect_to user_url(date: params[:date])
-  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
-    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
-    redirect_to attendances_edit_one_month_user_url(date: params[:date])
+  rescue ActiveRecord::RecordInvalid
+      flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+      redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
+  # 11.3.2 11.2付近参照 ※よく考えれば理解できそう!!
+  # def update_one_month
+  #   ActiveRecord::Base.transaction do # トランザクションを開始します。
+  #     attendances_params.each do |id, item|
+  #       attendance = Attendance.find(id)
+  #       attendance.update_attributes!(item)
+  #     end
+  #   end
+  #   flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+  #   redirect_to user_url(date: params[:date])
+  # rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
+  #   flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+  #   redirect_to attendances_edit_one_month_user_url(date: params[:date])
+  # end
   
   private
   
